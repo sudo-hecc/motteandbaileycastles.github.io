4ce975d1beb33bf50cdef05baa64012cb630afd4
Eesa Ayub <ay82.ee@gmail.com>
2025-12-11 18:31:34 +0000

    Create a new file containing html and link it

--- app.py
+++ app.py
@@ -1,28 +1,22 @@
 from flask import Flask, render_template_string
 import logging
+import os
 
+# INITIALISE WEB APPLICATION
 app = Flask(__name__)
 
-TEMPLATE = """
-<!doctype html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Motte and Bailey</title>
-  </head>
-  <body>
-    <h1>Motte and Bailey</h1>
-    <p>Motte and Bailey Castles were introduceced by King William of Englan after the Battle of Hastings (14 October 1066).</p>
-  </body>
-</html>
-"""
+# SETUP HTML TEMPLATE
+HERE = os.path.dirname("pages/")
 
+with open(os.path.join(HERE, "main_page.html"), "r") as f:
+    TEMPLATE = f.read()
 
+# DEFINE ROUTES
 @app.route("/")
 def index():
     return render_template_string(TEMPLATE)
 
-
+# RUN WEB APPLICATION
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     logging.info("Starting Waitress on 0.0.0.0:10000")
